6acd87e006e383dd4c464404b2d99fe30fc8e1f0
최진문 <rlawjsdlf13@naver.com>
2025-04-29 15:42:21 +0900

    Init: FE RN 개발환경세팅팅

---
+++ FE/daldidan/app_logic/features/camera/model/types.ts
@@ -0,0 +1,5 @@
+export interface CameraPermission {
+  granted: boolean;
+  canAskAgain: boolean;
+  expires: string;
+}
---
+++ FE/daldidan/app_logic/features/camera/ui/CameraScreen.tsx
@@ -0,0 +1,71 @@
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { toggleCameraFacing } from '../api/cameraApi';
+
+export default function CameraScreen() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return <View style={styles.container} />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>카메라 권한이 필요합니다.</Text>
+        <Button onPress={requestPermission} title='권한 요청' />
+      </View>
+    );
+  }
+
+  const handleToggleCameraFacing = () => {
+    setFacing(toggleCameraFacing);
+  };
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleToggleCameraFacing}
+          >
+            <Text style={styles.text}>카메라 전환</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+});
