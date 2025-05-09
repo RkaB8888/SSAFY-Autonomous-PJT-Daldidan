dd1e5b9873431f1e410f8a798f1a328425303f9a
정한균 <j1113019@naver.com>
2025-05-09 17:37:10 +0900

    Feat : 디스패처 구조 lazy import로 변경

--- BE/ai/services/predict_service.py
+++ BE/ai/services/predict_service.py
@@ -1,18 +1,26 @@
 # ai/services/predict_service.py
-from services.model_jhg1.predictor import predict_bytes as predict_jhg1
-from services.model_jhg2.predict.predictor import predict_bytes as predict_jhg2
-from services.model_a.predictor import predict as predict_a
-
-_MODEL_REGISTRY = {
-    "model_jhg1": predict_jhg1,
-    "model_jhg2": predict_jhg2,
-    "model_a": predict_a,
-}
 
 
 def predict(model_name: str, image_bytes: bytes):
-    try:
-        fn = _MODEL_REGISTRY[model_name]
-    except KeyError:
+    if model_name == "model_jhg1":
+        from services.model_jhg1.predictor import predict_bytes as predict_jhg1
+
+        return predict_jhg1(image_bytes)
+
+    elif model_name == "model_jhg2":
+        from services.model_jhg2.predict.predictor import predict_bytes as predict_jhg2
+
+        return predict_jhg2(image_bytes)
+
+    elif model_name == "model_a":
+        from services.model_a.predictor import predict as predict_a
+
+        return predict_a(image_bytes)
+
+    elif model_name == "model_b":
+        from services.model_b.predictor import predict as predict_b
+
+        return predict_b(image_bytes)
+
+    else:
         raise ValueError(f"Unknown model: {model_name!r}")
-    return fn(image_bytes)
