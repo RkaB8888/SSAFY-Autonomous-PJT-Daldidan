18287691436f783af9b2eb9da0ea1e8b446395fb
최진문 <rlawjsdlf13@naver.com>
2025-05-09 12:24:18 +0900

    Refactor: 불필요 폴더 및 파일 삭제&정리

--- FE/daldidan/app/_layout.tsx
+++ FE/daldidan/app/_layout.tsx
@@ -1,4 +1,8 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
@@ -19,11 +23,11 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='index' />
+        <Stack.Screen name='+not-found' />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </ThemeProvider>
   );
 }
--- FE/daldidan/app/index.tsx
+++ FE/daldidan/app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CameraViewNoDetect from '../../components/CameraViewNoDetect';
+import CameraViewNoDetect from '../components/CameraViewNoDetect';
 
 export default function App() {
   // 메인 / 근접 시 / 원거리 시 카메라 뷰 분기처리를 해야함
