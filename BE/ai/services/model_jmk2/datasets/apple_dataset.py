Fix : GPU기반 모델(scaling추가가)

BE/ai/services/model_jmk2/datasets/apple_dataset.py
@@ -99,12 +99,15 @@ from PIL import Image
 from torchvision import transforms
 import numpy as np
 from features.extract_features import extract_features
+import joblib
 
 class AppleDataset(Dataset):
     def __init__(self, image_dir, json_files, transform=None):
         self.image_dir = image_dir
         self.json_files = json_files
         self.transform = transform
+        # 서버 내 저장된 scaler load
+        self.scaler = joblib.load("/home/j-k12e206/ai-hub/scaler.pkl")
 
     def __len__(self):
         return len(self.json_files)
@@ -130,8 +133,10 @@ class AppleDataset(Dataset):
         mask = np.zeros((img_h, img_w), dtype=np.uint8)
         cv2.fillPoly(mask, [points], 255)
 
-        manual_features = extract_features(image, mask)
-        manual_features = torch.tensor(manual_features, dtype=torch.float32)
+        # manual feature 추출 → scaler로 scaling
+        manual_features_raw = extract_features(image, mask)
+        manual_features_scaled = self.scaler.transform([manual_features_raw])[0]
+        manual_features = torch.tensor(manual_features_scaled, dtype=torch.float32)
 
         image_pil = Image.open(img_path).convert("RGB")
         if self.transform:

BE/ai/services/model_jmk2/train.py
@@ -167,6 +167,11 @@ batch_size = 64
 num_epochs = 20
 lr = 0.001
 
+def custom_collate(batch):
+    batch = [b for b in batch if b is not None]
+    return torch.utils.data.dataloader.default_collate(batch)
+
+
 json_files = [os.path.join(JSON_DIR, f) for f in os.listdir(JSON_DIR) if f.endswith('.json')]
 print(f"✅ json_files 개수: {len(json_files)}개")
 
@@ -182,8 +187,10 @@ val_size = int(len(dataset) * val_split)
 train_size = len(dataset) - val_size
 train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=32, pin_memory=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=32, pin_memory=True)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                          num_workers=32, pin_memory=True, collate_fn=custom_collate)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False,
+                        num_workers=32, pin_memory=True, collate_fn=custom_collate)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = FusionModel(manual_feature_dim).to(device)