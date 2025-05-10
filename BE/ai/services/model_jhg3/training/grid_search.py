bc94d164ab0bb67f1362f18363121f870c4b514e
정한균 <j1113019@naver.com>
2025-05-10 18:53:57 +0900

    Fix : evaluate 함수 오타 수정

--- BE/ai/services/model_jhg3/training/grid_search.py
+++ BE/ai/services/model_jhg3/training/grid_search.py
@@ -37,7 +37,7 @@ def evaluate(y_true, y_pred):
     from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
     mae = mean_absolute_error(y_true, y_pred)
-    rmse = (np.sqrt(mean_squared_error(y_true, y_pred)),)
+    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
     r2 = r2_score(y_true, y_pred)
     return mae, rmse, r2
 
