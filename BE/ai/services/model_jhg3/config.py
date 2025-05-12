81f4c3cdf0a02c91fca66a40bba0c19b358b6716
정한균 <j1113019@naver.com>
2025-05-12 12:28:17 +0900

    Refactor :  로컬에서 실행 준비

--- BE/ai/services/model_jhg3/config.py
+++ BE/ai/services/model_jhg3/config.py
@@ -2,11 +2,20 @@
 from pathlib import Path
 
 # ── GPU 서버 고정 절대경로 ───────────────────────────────
-TRAIN_ROOT = Path("/home/j-k12e206/ai-hub/Fuji/train")
+# TRAIN_ROOT = Path("/home/j-k12e206/ai-hub/Fuji/train")
+# IMAGES_DIR = TRAIN_ROOT / "images"
+# JSONS_DIR = TRAIN_ROOT / "jsons"
+
+# VALID_ROOT = Path("/home/j-k12e206/ai-hub/Fuji/valid")
+# VALID_IMAGES_DIR = VALID_ROOT / "images"
+# VALID_JSONS_DIR = VALID_ROOT / "jsons"
+
+# ──  Local 고정 절대경로 ───────────────────────────────
+TRAIN_ROOT = Path("C:\Users\SSAFY\Desktop\SSAFY-Autonomous-PJT\dataset\ai-hub\Fuji\train")
 IMAGES_DIR = TRAIN_ROOT / "images"
 JSONS_DIR = TRAIN_ROOT / "jsons"
 
-VALID_ROOT = Path("/home/j-k12e206/ai-hub/Fuji/valid")
+VALID_ROOT = Path("C:\Users\SSAFY\Desktop\SSAFY-Autonomous-PJT\dataset\ai-hub\Fuji\valid")
 VALID_IMAGES_DIR = VALID_ROOT / "images"
 VALID_JSONS_DIR = VALID_ROOT / "jsons"
 
@@ -23,4 +32,4 @@ MODEL_SAVE_PATH = WEIGHTS_DIR / "lightgbm_cnn.pkl"  # ✅ 파일명 통일
 # ── 처리 옵션 플래그 ─────────────────────────────────────────
 USE_NIR = True  # False->sugar_content만, True->NIR fallback
 USE_SEGMENTATION = True  # True->segmentation, False->bbox
-EMBEDDING_MODE = "handcrafted"  # 'cnn' 또는 'handcrafted'
+EMBEDDING_MODE = "cnn"  # 'cnn' 또는 'handcrafted'
