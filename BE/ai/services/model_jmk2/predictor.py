fecf4de54525cb9f8de9fbe69665de8d88f03b07
전민경 <chfhddlfhqls@naver.com>
2025-05-12 23:57:07 +0900

    Feat : 이미지 추론 predict_bytes 함수 추가가

---
+++ BE/ai/services/model_jmk2/predictor.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import torch
+from PIL import Image
+from .model_loader import model, scaler, transform
+from .features.extract_features import extract_fast_features
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def predict_bytes(image_bytes: bytes) -> float:
+    try:
+        np_arr = np.frombuffer(image_bytes, np.uint8)
+        img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        if img is None:
+            raise ValueError("이미지 디코딩 실패")
+
+        h, w = img.shape[:2]
+        mask = np.ones((h, w), dtype=np.uint8) * 255
+
+        manual_feat = extract_fast_features(img, mask)
+        manual_feat = scaler.transform([manual_feat])[0]
+        manual_feat_tensor = torch.tensor(manual_feat, dtype=torch.float32).unsqueeze(0).to(device)
+        image_tensor = transform(img).unsqueeze(0).to(device)
+
+        with torch.no_grad():
+            pred = model(image_tensor, manual_feat_tensor).squeeze().item()
+
+        return round(pred, 2)
+
+    except Exception as e:
+        print(f"[ERROR] predict_bytes 실패: {e}")
+        raise e
