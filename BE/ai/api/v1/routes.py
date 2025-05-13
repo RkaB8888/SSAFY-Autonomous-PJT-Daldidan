8754bc5796b4a1ae72f75b4bbda14a0ecb73c28f
전민경 <chfhddlfhqls@naver.com>
2025-05-13 11:23:24 +0900

    Feat : 이미지 추론 연결경로 추가

--- BE/ai/api/v1/routes.py
+++ BE/ai/api/v1/routes.py
@@ -11,11 +11,20 @@ async def health_check():
     return {"status": "AI server is running"}
 
 
+# @router.post("/predict", response_model=PredictResponse)
+# async def predict_image(id: int = Form(...), image: UploadFile = File(...)):
+#     try:
+#         image_bytes = await image.read()
+#         result = predict("model_jhg2", image_bytes)  # ← 바이트 전달
+#         return PredictResponse(id=id, predict_sugar_content=float(result["confidence"]))
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+
 @router.post("/predict", response_model=PredictResponse)
 async def predict_image(id: int = Form(...), image: UploadFile = File(...)):
     try:
         image_bytes = await image.read()
-        result = predict("model_jhg2", image_bytes)  # ← 바이트 전달
-        return PredictResponse(id=id, predict_sugar_content=float(result["confidence"]))
+        result = predict("model_jmk2", image_bytes)  # ← 바이트 전달
+        return PredictResponse(id=id, predict_sugar_content=float(result))
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
--- BE/ai/services/predict_service.py
+++ BE/ai/services/predict_service.py
@@ -21,6 +21,12 @@ def predict(model_name: str, image_bytes: bytes):
         from services.model_b.predictor import predict as predict_b
 
         return predict_b(image_bytes)
+    
+        
+    elif model_name == "model_jmk2":
+        from services.model_jmk2.predictor import predict_bytes as predict_jmk2
+        
+        return predict_jmk2(image_bytes)
 
     else:
         raise ValueError(f"Unknown model: {model_name!r}")
